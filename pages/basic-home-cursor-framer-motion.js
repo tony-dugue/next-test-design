b560c71108d6822fac938f9213c7579b0e7ee480
tony dugué <duguetony@gmail.com>
2022-07-14 10:51:48 +0200

    add page with basic home cursor animated with framer motion

---
+++ pages/basic-home-cursor-framer-motion.js
@@ -0,0 +1,65 @@
+import {useEffect, useState} from "react";
+import { motion } from 'framer-motion'
+import styles from '../styles/basic-home-cursor-framer-motion.module.scss'
+
+const BasicHomeCursorFramerMotion = () => {
+
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  const [cursorVariant, setCursorVariant] = useState("default")
+
+  const handleMouseMove = e => {
+    setMousePosition({ x: e.clientX, y: e.clientY })
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 10,
+      y: mousePosition.y - 10
+    },
+    text: {
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      height: 150,
+      width: 150,
+      backgroundColor: "rgb(3,150, 166)",
+      mixBlendMode: "difference"
+    },
+    description: {
+      x: mousePosition.x - 40,
+      y: mousePosition.y - 40,
+      height: 80,
+      width: 80,
+      backgroundColor: "rgb(3,73, 85)",
+      mixBlendMode: "difference"
+    }
+  }
+
+  const textEnter = () => setCursorVariant("text")
+  const textLeave = () => setCursorVariant("default")
+
+  const descriptionEnter = () => setCursorVariant("description")
+  const descriptionLeave = () => setCursorVariant("default")
+
+  return (
+    <div>
+      <div className={styles.container}>
+        <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className={styles.title}>Hello World</h1>
+
+        <p  className={styles.description}>
+          Leading <span onMouseEnter={descriptionEnter} onMouseLeave={descriptionLeave}>digital agency</span> with solid design and development
+          expertise. We build readymade websites, mobile applications, and
+          elaborate online <span onMouseEnter={descriptionEnter} onMouseLeave={descriptionLeave}>business services</span>.
+        </p>
+
+        <motion.div className={styles.cursor} variants={variants} animate={cursorVariant} />
+      </div>
+    </div>
+  )
+}
+
+export default BasicHomeCursorFramerMotion
--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,8 @@ export default function Home() {
       <main className={styles.main}>
         <Link href="/gsap-animation-basic"><p className={styles.link}>GSAP Animation (basic)</p></Link>
         <Link href="/smooth-scrolling"><p  className={styles.link}>Smooth scrolling (with Smooth-scrollbar library)</p></Link>
+        <Link href="/cuberto-home-cursor"><p  className={styles.link}>Cuberto home cursor</p></Link>
+        <Link href="/basic-home-cursor-framer-motion"><p  className={styles.link}>Basic home cursor (with framer motion)</p></Link>
       </main>
 
     </div>
